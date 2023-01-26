ec48b9cd0eadb15379533a8067b791aa78c993f3
rm-Mungai-f <mungai.mbugua1409@gmail.com>
2023-01-26 11:17:44 +0300

    modify files

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact=()=>{
+    return(
+        <div className="ui raised very padded text container segment" style={{marginTop:'80px'}}>
+            <h3 className="ui header">Contact</h3>
+            <p>LOREM IPSUM</p>
+        </div>
+    );
+}
+
+export default Contact;
