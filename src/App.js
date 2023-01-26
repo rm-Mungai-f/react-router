88055e9fe51ab935232070e6da0c1df9aa5169b6
rm-Mungai-f <mungai.mbugua1409@gmail.com>
2023-01-26 12:41:30 +0300

    update routes for my project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App(){
     return(
+        <BrowserRouter>
         <div className="App">
             <Navbar/>
-            App
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/contact" element={<Contact/>}/>
+            </Routes>
         </div>
+        </BrowserRouter>
     );
 }
 
