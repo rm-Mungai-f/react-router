39cdaa8e0cb9770fa39aeeec103621abdc13f616
rm-Mungai-f <mungai.mbugua1409@gmail.com>
2023-01-27 11:50:11 +0300

    modified navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar=()=>{
     return(
         <nav className="ui raised very padded segment">
             <a href="/" className="ui teal inverted segment">Navbar</a>
             <div className="ui right floated header">
-                <button className="ui button"><a href="/">Home</a></button>
-                <button className="ui button"><a href="/about">About</a></button>
-                <button className="ui button"><a href="/contact">Contact</a></button>
+                <button className="ui button"><NavLink to="/">Home</NavLink></button>
+                <button className="ui button"><NavLink to="/about">About</NavLink></button>
+                <button className="ui button"><NavLink to="/contact">Contact</NavLink></button>
             </div>
         </nav>
     );
